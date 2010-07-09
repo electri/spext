ddee6d39922135e0b0e900dd066e0a1b3db57e25
monkeycn@gmail.com <monkeycn@gmail.com@01c74763-b75b-5a55-715c-6f18d641bb61>
2010-07-09 08:05:19 +0000

    日志增加了最大大小分段

--- trunk/include/sp_ext_log.h
+++ trunk/include/sp_ext_log.h
@@ -62,27 +62,34 @@ private:
 class sp_ext_log
 {
 public:
+    enum { max_buf_size = 30 * 1024 ,
+           max_file_size = 10 * 1024, // 100 Mb, 100*1024 k
+         };
 	enum e_log_enum
 	{
 		e_log_enum_none = 0, /// 不打日志
 		// 客户自能用以下枚举
 		e_log_enum_err,/// 错误
-        e_log_enum_debug,/// 调试
 		e_log_enum_warn,/// 警告
 		e_log_enum_info,/// 信息
+        e_log_enum_debug,/// 调试
 
 	};
 public:
     sp_ext_log();
     virtual ~sp_ext_log();
 public:
-    BOOL init_log(int level, bool print_to_console = false, const char* log_file = NULL);
+    BOOL init_log(int level, bool print_to_console, int max_size, const char* log_file = NULL);
     BOOL un_init_log();
 public:
-    void log(int log_level, const char* logStr);
-    void log_fmt(int log_level, const char* fmt,...);
+    void log(int log_level, const char* log_str);
+    bool log_fmt(int log_level, const char* fmt,...); // 返回是否超出缓冲区
 protected:
     void write_log(int log_level, const char* buf,int len);
+    static const char* get_level_pri(int log_level);
+    static bool find_file(const char* file_name);
+    static std::string get_curr_date_str(int try_index);// 2010-06-27(00),2010-06-27(01),
+    void rename_logfile(); // 重命名日志文件
 protected:
     HANDLE file_handle_;
     CRITICAL_SECTION crit_sec_;
@@ -95,9 +102,9 @@ protected:
 };
 
 #define SP_LOG_ERR (sp_ext::sp_ext_log::e_log_enum_err)
-#define SP_LOG_DEBUG (sp_ext::sp_ext_log::e_log_enum_debug)
 #define SP_LOG_WARN (sp_ext::sp_ext_log::e_log_enum_warn)
 #define SP_LOG_INFO (sp_ext::sp_ext_log::e_log_enum_info)
+#define SP_LOG_DEBUG (sp_ext::sp_ext_log::e_log_enum_debug)
 
 }/// namespace sp_ext
 #endif /// __SP_EXT_LOG_H__
